ffc665ef522b8b95973068c914c23aa5656b098e
Daniel Larsson <dlo@qlik.com>
2020-04-02 10:27:01 +0200

    Add test case

--- scheduler/simple_test.go
+++ scheduler/simple_test.go
@@ -1,9 +1,17 @@
 package scheduler
 
 import (
+	"context"
 	"testing"
+	"time"
+
+	"github.com/qlik-oss/gopherciser/action"
+	"github.com/qlik-oss/gopherciser/scenario"
+	"github.com/qlik-oss/gopherciser/session"
+	"github.com/qlik-oss/gopherciser/users"
 
 	"github.com/pkg/errors"
+	"github.com/qlik-oss/gopherciser/connection"
 )
 
 func TestSimpleSched(t *testing.T) {
@@ -44,5 +52,96 @@ func TestSimpleSched(t *testing.T) {
 	if err := errors.Cause(sched.Validate()); err != nil {
 		t.Log(err)
 		t.Error("validation failed")
+	}
+}
+
+type TestOnlyInstanceSeedAction struct {
+	Result []int
+}
+
+func (settings TestOnlyInstanceSeedAction) Validate() error {
+	return nil
+}
+func (settings *TestOnlyInstanceSeedAction) Execute(sessionState *session.State,
+	actionState *action.State, connectionSettings *connection.ConnectionSettings, label string, reset func()) {
+	settings.Result = append(settings.Result, sessionState.Randomizer().Rand(100))
+}
+
+func TestOnlyInstanceSeed(t *testing.T) {
+
+	iterations := 2
+	sched1 := &SimpleScheduler{
+		Scheduler: Scheduler{
+			SchedType:          SchedSimple,
+			InstanceNumber:     2,
+			connectionSettings: &connection.ConnectionSettings{},
+		},
+		Settings: SimpleSchedSettings{
+			ExecutionTime:    -1,
+			Iterations:       iterations,
+			RampupDelay:      0.1,
+			ConcurrentUsers:  1,
+			ReuseUsers:       false,
+			OnlyInstanceSeed: true,
+		},
+	}
+
+	deRef := *sched1
+	sched2 := &deRef
+	sched2.Settings.OnlyInstanceSeed = false
+
+	actionSettings := &TestOnlyInstanceSeedAction{}
+	testAction := scenario.Action{ActionCore: scenario.ActionCore{}, Settings: actionSettings}
+
+	actionsToAdd := 10
+
+	actions := make([]scenario.Action, 0, actionsToAdd)
+	for i := 0; i < actionsToAdd; i++ {
+		actions = append(actions, testAction)
+	}
+
+	runUserIteration(t, sched1, actions)
+	runUserIteration(t, sched2, actions)
+
+	result := actionSettings.Result
+	t.Log("results:", result)
+
+	// check we have to correct amount of results
+	if len(result) != actionsToAdd*iterations*2 {
+		t.Fatalf("results<%v> not of expected length<%d>", result, actionsToAdd*iterations)
+	}
+
+	// sched 1 iteration 1 and 2 sequences should be the same
+	seq11 := result[0:actionsToAdd]
+	seq12 := result[actionsToAdd:(actionsToAdd * 2)]
+	for i := 0; i < actionsToAdd; i++ {
+		if seq11[i] != seq12[i] {
+			t.Fatalf("iteration sequences differs although using OnlyInstanceSeed flag, sequences: 1<%v> 2<%v>", seq11, seq12)
+		}
+	}
+
+	// sched 1 results should differ from sched 2 results
+	seq1 := result[0:(actionsToAdd * iterations)]
+	seq2 := result[(actionsToAdd * iterations):(actionsToAdd * iterations * 2)]
+	differenceFound := false
+	for i := 0; i < actionsToAdd*iterations; i++ {
+		if seq1[i] != seq2[i] {
+			differenceFound = true
+			break
+		}
+	}
+	if !differenceFound {
+		t.Fatalf("scheduler sequences doesn't differ although when using OnlyInstanceSeed and not, sequences: 1<%v> 2<%v>", seq1, seq2)
+	}
+}
+
+func runUserIteration(t *testing.T, sched *SimpleScheduler, actions []scenario.Action) {
+	t.Helper()
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
+	defer cancel()
+
+	if err := sched.iteratorNewUsers(ctx, time.Minute, nil, actions, "", users.NewUserGeneratorNone()); err != nil {
+		t.Fatal(err)
 	}
 }
