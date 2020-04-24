7ba9d59f71cee0653d084aa3ce701abbb6f6faf3
Daniel Larsson <dlo@qlik.com>
2020-04-24 16:50:54 +0200

    first iteration of a structure file

--- config/getappstructure.go
+++ config/getappstructure.go
@@ -2,18 +2,48 @@ package config
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"github.com/pkg/errors"
+	"github.com/qlik-oss/enigma-go"
 	"github.com/qlik-oss/gopherciser/action"
 	"github.com/qlik-oss/gopherciser/connection"
 	"github.com/qlik-oss/gopherciser/scenario"
 	"github.com/qlik-oss/gopherciser/scheduler"
+	"github.com/qlik-oss/gopherciser/senseobjects"
 	"github.com/qlik-oss/gopherciser/session"
+	"os"
+	"sync"
 	"time"
 )
 
 type (
 	getAppStructureSettings struct{}
+
+	// AppObjectDef title and ID of a Sense object
+	AppObjectDef struct {
+		// Id of object
+		Id string
+		// Type of Sense object
+		Type string
+	}
+
+	// AppStructureObject sense object structure
+	AppStructureObject struct {
+		AppObjectDef
+		// Properties of Sense object
+		Properties json.RawMessage
+		// Children to the sense object
+		Children []AppObjectDef
+	}
+
+	// AppStructure of Sense app
+	AppStructure struct {
+		// Objects in Sense app
+		Objects []AppStructureObject
+
+		structureLock sync.Mutex
+	}
 )
 
 func (cfg *Config) getAppStructureScenario() []scenario.Action {
@@ -100,12 +130,138 @@ func (cfg *Config) GetAppStructures(ctx context.Context) error {
 	return nil
 }
 
-// Execute implements ActionSettings interface
-func (settings *getAppStructureSettings) Execute(sessionState *session.State, actionState *action.State, connectionSettings *connection.ConnectionSettings, label string, reset func()) {
-	fmt.Println("Getting app structure...")
-}
-
 // Validate implements ActionSettings interface
 func (settings *getAppStructureSettings) Validate() error {
 	return nil
+}
+
+// Execute implements ActionSettings interface
+func (settings *getAppStructureSettings) Execute(sessionState *session.State, actionState *action.State, connectionSettings *connection.ConnectionSettings, label string, reset func()) {
+	if sessionState.Connection == nil || sessionState.Connection.Sense() == nil {
+		actionState.AddErrors(errors.New("Not connected to a Sense environment"))
+		return
+	}
+
+	app := sessionState.Connection.Sense().CurrentApp
+	if app == nil {
+		actionState.AddErrors(errors.New("Not connected to a Sense app"))
+		return
+	}
+
+	var allInfos []*enigma.NxInfo
+
+	if err := sessionState.SendRequest(actionState, func(ctx context.Context) error {
+		var err error
+		allInfos, err = app.Doc.GetAllInfos(ctx)
+		return err
+	}); err != nil {
+		actionState.AddErrors(err)
+		return
+	}
+
+	appStructure := &AppStructure{}
+
+	for _, info := range allInfos {
+		if info == nil {
+			continue
+		}
+		if err := getStructureForObjectAsync(sessionState, actionState, app, info.Id, info.Type, appStructure); err != nil {
+			actionState.AddErrors(err)
+			return
+		}
+	}
+
+	if sessionState.Wait(actionState) {
+		return // An error occurred
+	}
+
+	raw, err := json.MarshalIndent(appStructure, "", "  ")
+	if err != nil {
+		actionState.AddErrors(errors.Wrap(err, "error marshaling app structure"))
+		return
+	}
+
+	outputDir := sessionState.OutputsDir
+	if outputDir != "" && outputDir[len(outputDir)-1:] != "/" {
+		outputDir += "/"
+	}
+
+	structureFile, err := os.Create(fmt.Sprintf("%s%s.structure", outputDir, app.GUID))
+	if err != nil {
+		actionState.AddErrors(errors.Wrap(err, "failed to create structure file"))
+		return
+	}
+	defer func() {
+		if err := structureFile.Close(); err != nil {
+			_, _ = fmt.Fprintf(os.Stderr, "failed to close file<%v> successfully: %v\n", structureFile, err)
+		}
+	}()
+	if _, err = structureFile.Write(raw); err != nil {
+		actionState.AddErrors(errors.Wrap(err, "error while writing to structure file"))
+		return
+	}
+}
+
+func getStructureForObjectAsync(sessionState *session.State, actionState *action.State, app *senseobjects.App, id, typ string, appStructure *AppStructure) error {
+	// Todo check if visualization
+	// Todo get dimensions and measures
+	// Todo check for master item connections
+
+	if appStructure == nil {
+		return errors.New("appStructure object is nil")
+	}
+
+	sessionState.QueueRequest(func(ctx context.Context) error {
+		obj := AppStructureObject{
+			AppObjectDef: AppObjectDef{
+				Id:   id,
+				Type: typ,
+			},
+		}
+
+		genObj, err := app.Doc.GetObject(ctx, id)
+		if err != nil {
+			return errors.WithStack(err)
+		}
+
+		obj.Properties, err = genObj.GetPropertiesRaw(ctx)
+		if err != nil {
+			return errors.WithStack(err)
+		}
+
+		childInfos, err := genObj.GetChildInfos(ctx)
+		if err != nil {
+			return errors.WithStack(err)
+		}
+
+		var children []AppObjectDef
+		if len(childInfos) > 0 {
+			children = make([]AppObjectDef, 0, len(childInfos))
+		}
+
+		for _, child := range childInfos {
+			if child == nil {
+				continue
+			}
+			children = append(children, AppObjectDef{
+				Id:   child.Id,
+				Type: child.Type,
+			})
+		}
+		obj.Children = children
+
+		appStructure.AddObject(obj)
+		return nil
+	}, actionState, true, "")
+
+	return nil
+}
+
+func (structure *AppStructure) AddObject(obj AppStructureObject) {
+	structure.structureLock.Lock()
+	defer structure.structureLock.Unlock()
+	if structure.Objects == nil {
+		structure.Objects = make([]AppStructureObject, 0, 1)
+	}
+	structure.Objects = append(structure.Objects, obj)
 }
