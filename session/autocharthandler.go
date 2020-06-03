a04d697a11cf3fb544ecad94193b5e7ccead788a
Daniel Larsson <dlo@qlik.com>
2020-06-03 15:23:56 +0200

    avoid Get data warning for auto-chart main object

--- session/autocharthandler.go
+++ session/autocharthandler.go
@@ -2,6 +2,7 @@ package session
 
 import (
 	"context"
+	"github.com/pkg/errors"
 	"github.com/qlik-oss/enigma-go"
 	"github.com/qlik-oss/gopherciser/action"
 	"github.com/qlik-oss/gopherciser/enigmahandlers"
@@ -23,9 +24,33 @@ func (handler *AutoChartHandler) Instance(id string) ObjectHandlerInstance {
 
 // GetObject implement ObjectHandler interface
 func (instance *AutoChartInstance) SetObjectAndEvents(sessionState *State, actionState *action.State, obj *enigmahandlers.Object, genObj *enigma.GenericObject) {
-	sessionState.QueueRequest(func(ctx context.Context) error {
-		return GetObjectLayout(sessionState, actionState, obj)
-	}, actionState, true, "")
-
+	instance.GetObjectLayout(sessionState, actionState, obj)
 	handleAutoChart(sessionState, actionState, genObj, obj)
+}
+
+// GetObjectLayout for auto-chart main object
+func (instance *AutoChartInstance) GetObjectLayout(sessionState *State, actionState *action.State, obj *enigmahandlers.Object) {
+	sessionState.QueueRequest(func(ctx context.Context) error {
+		enigmaObject, ok := obj.EnigmaObject.(*enigma.GenericObject)
+		if !ok {
+			return errors.Errorf("Failed to cast object<%s> to *enigma.GenericObject", obj.ID)
+		}
+		sessionState.LogEntry.LogDebugf("Getting layout for object<%s> handle<%d> type<%s> START", obj.ID, obj.Handle, enigmaObject.GenericType)
+
+		rawLayout, layoutErr := sessionState.SendRequestRaw(actionState, enigmaObject.GetLayoutRaw)
+		if layoutErr != nil {
+			return errors.Wrapf(layoutErr, "object<%s>.GetLayout", enigmaObject.GenericId)
+		}
+
+		if err := SetChildList(rawLayout, obj); err != nil {
+			return errors.Wrapf(err, "failed to get childlist for object<%s> type<%s>", obj.ID, enigmaObject.GenericType)
+		}
+		if err := SetChildren(rawLayout, obj); err != nil {
+			return errors.Wrapf(err, "failed to get children for object<%s> type<%s>", obj.ID, enigmaObject.GenericType)
+		}
+
+		sessionState.LogEntry.LogDebugf("Getting layout for object<%s> handle<%d> type<%s> END", obj.ID, obj.Handle, enigmaObject.GenericType)
+
+		return nil
+	}, actionState, true, "")
 }
