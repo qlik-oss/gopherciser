9b0201a2834d0458ce5d733828eb6d19387588c0
Daniel Larsson <dlo@qlik.com>
2020-10-05 12:57:37 +0200

    Remove QSEoK examples and references since no longer valid after 1.62.67. (#119)
    * Remove QSEoK examples and references since no longer valid after 1.62.67
    * Rename QSEfW to QSEoW

--- docs/settingup.md
+++ docs/settingup.md
@@ -339,7 +339,7 @@ All actions follow the same basic structure:
 
 # Common actions
 
-These actions are applicable to both Qlik Sense Enterprise for Windows (QSEfW) and Qlik Sense Enterprise on Kubernetes (QSEoK) deployments.
+These actions are applicable to both Qlik Sense Enterprise for Windows (QSEoW) and Qlik Sense Enterprise on Kubernetes (QSEoK) deployments.
 
 **Note:** It is recommended to prepend the actions listed here with an `openapp` action as most of them perform operations in an app context (such as making selections or changing sheets).
 
--- generatedocs/generated/documentation.go
+++ generatedocs/generated/documentation.go
@@ -404,7 +404,7 @@ var (
                 Title: "Common actions",
                 Actions: []string{ "applybookmark","changesheet","clearall","clickactionbutton","createbookmark","createsheet","deletebookmark","deletesheet","disconnectapp","duplicatesheet","iterated","listboxselect","openapp","productversion","publishbookmark","publishsheet","randomaction","reload","select","setscript","sheetchanger","staticselect","subscribeobjects","thinktime","unpublishbookmark","unpublishsheet","unsubscribeobjects" },
                 DocEntry: common.DocEntry{
-                    Description: "# Common actions\n\nThese actions are applicable to both Qlik Sense Enterprise for Windows (QSEfW) and Qlik Sense Enterprise on Kubernetes (QSEoK) deployments.\n\n**Note:** It is recommended to prepend the actions listed here with an `openapp` action as most of them perform operations in an app context (such as making selections or changing sheets).\n",
+                    Description: "# Common actions\n\nThese actions are applicable to both Qlik Sense Enterprise for Windows (QSEoW) and Qlik Sense Enterprise on Kubernetes (QSEoK) deployments.\n\n**Note:** It is recommended to prepend the actions listed here with an `openapp` action as most of them perform operations in an app context (such as making selections or changing sheets).\n",
                     Examples: "",
                 },
             },
