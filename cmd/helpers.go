f4489fdcc89f4800aa21844f6f4ee66c1c7eaa31
Daniel Larsson <dlo@qlik.com>
2020-11-02 15:35:21 +0100

    support config file from stdin

--- cmd/helpers.go
+++ cmd/helpers.go
@@ -1,6 +1,7 @@
 package cmd
 
 import (
+	"bufio"
 	"fmt"
 	"io/ioutil"
 	"os"
@@ -14,7 +15,12 @@ import (
 )
 
 var (
+	// parameters for config file
 	cfgFile string
+
+	// parameters for overrides
+	scriptOverrides    []string
+	scriptOverrideFile string
 
 	// parameters for logging
 	traffic        bool
@@ -53,13 +59,30 @@ func AddLoggingParameters(cmd *cobra.Command) {
 }
 
 func unmarshalConfigFile() (*config.Config, error) {
+	var cfgJSON []byte
+	var err error
 	if cfgFile == "" {
-		return nil, errors.Errorf("No config file defined")
-	}
+		hasPipe, err := hasPipe()
+		if err != nil {
+			return nil, errors.Wrap(err, "error reading config from stdin")
+		}
+		if !hasPipe {
+			return  nil, errors.New("no config file and nothing on stdin")
+		}
 
-	cfgJSON, err := ioutil.ReadFile(cfgFile)
-	if err != nil {
-		return nil, errors.Wrapf(err, "Error reading config from file<%s>", cfgFile)
+		scanner := bufio.NewScanner(os.Stdin)
+		for scanner.Scan() {
+			b := scanner.Bytes()
+			if cfgJSON == nil {
+				cfgJSON = make([]byte, 0, len(b))
+			}
+			cfgJSON = append(cfgJSON, b...)
+		}
+	} else {
+		cfgJSON, err = cfgJsonFromFile()
+		if err != nil {
+			return nil, errors.Wrapf(err, "Error reading config from file<%s>", cfgFile)
+		}
 	}
 
 	var overrides []string
@@ -78,6 +101,14 @@ func unmarshalConfigFile() (*config.Config, error) {
 	}
 
 	return &cfg, nil
+}
+
+func cfgJsonFromFile() ([]byte, error) {
+	if cfgFile == "" {
+		return nil, errors.Errorf("No config file defined")
+	}
+
+	return ioutil.ReadFile(cfgFile)
 }
 
 func overrideScriptValues(cfgJSON []byte) ([]byte, []string, error) {
