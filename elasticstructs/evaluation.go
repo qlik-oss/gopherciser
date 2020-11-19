fb8fde4e16406ea6a9beb4003c908ed0eeaabbd9
Daniel Larsson <dlo@qlik.com>
2020-11-19 15:52:33 +0100

    update elastic openhub for api compliance (#146)
    Update elastic open hub action for API compliance

--- elasticstructs/evaluation.go
+++ elasticstructs/evaluation.go
@@ -4,6 +4,8 @@ type (
 	EvaluationProperties struct {
 		SpaceID string `json:"spaceId,omitempty"`
 		Owner   string `json:"owner,omitempty"`
+		OwnerID string `json:"ownerId,omitempty"`
+		UserID  string `json:"usreId,omitempty"`
 		Type    string `json:"type,omitempty"`
 	}
 
