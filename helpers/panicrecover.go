8952b3d62c375157765680c01282604268fb1e04
Daniel Larsson <dlo@qlik.com>
2021-02-15 14:17:29 +0100

    log panics with stack

--- helpers/panicrecover.go
+++ helpers/panicrecover.go
@@ -1,21 +1,15 @@
 package helpers
 
 import (
+	"runtime/debug"
+
 	"github.com/pkg/errors"
 )
 
 // RecoverWithError recovers from panic and sets error to panicErr
 func RecoverWithError(panicErr *error) {
 	if r := recover(); r != nil {
-		var ok bool
-		var err error
-
-		if err, ok = r.(error); !ok {
-			err = errors.Errorf("PANIC: %+v", r)
-		}
-		if panicErr != nil {
-			*panicErr = err
-		}
+		*panicErr = errors.Errorf("PANIC: %+v Stack: %s", r, debug.Stack())
 	}
 }
 
