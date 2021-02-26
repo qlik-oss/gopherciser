481f350ea1d6611c571dc3a9c98d175cf6106d4e
Anton Ljungberg <anton.ljungberg@qlik.com>
2021-02-26 16:50:10 +0100

    Add apps to artifactmap
    ... with option to keep current apps in artifactmap

--- scenario/elasticmoveapp.go
+++ scenario/elasticmoveapp.go
@@ -21,6 +21,8 @@ type (
 	}
 
 	NavigateToSpaceSettings struct {
+		// KeepCurrent artifact map and fill with results from explore action
+		KeepCurrent          bool `json:"keepcurrent" displayname:"Keep current" doc-key:"elasticmoveapp.keepcurrent"`
 		DoNotNavigateToSpace bool `json:"donotnavigatetospace" displayname:"Navigate to space" doc-key:"elasticmoveapp.donotnavigatetospace"`
 	}
 
@@ -102,17 +104,26 @@ func (settings ElasticMoveAppSettings) Execute(sessionState *session.State, acti
 		actionState.AddErrors(errors.Errorf("unexpected response code <%d> when putting app in new space: %s", putApp.ResponseStatusCode, putApp.ResponseBody))
 	}
 	settings.NavigateToSpace(sessionState, actionState, host, destSpace.ID)
+	sessionState.Wait(actionState)
 }
 
 func (settings NavigateToSpaceSettings) NavigateToSpace(sessionState *session.State, actionState *action.State, host string, spaceID string) {
 	if settings.DoNotNavigateToSpace {
 		return
 	}
+	if !settings.KeepCurrent {
+		sessionState.ArtifactMap.ClearArtifactMap()
+	}
 	_, err := sessionState.Rest.GetSync(fmt.Sprintf("%s/api/v1/spaces/%s/assignments?limit=100", host, spaceID), actionState, sessionState.LogEntry, nil)
 	if err != nil {
 		actionState.AddErrors(errors.Wrap(err, "failed during navigate to space"))
 		return
 	}
+	itemsReq, err := sessionState.Rest.GetSync(
+		fmt.Sprintf("%s/api/v1/items?sort=-updatedAt&limit=24&spaceId=%s&resourceType=app,qvapp,qlikview,genericlink,sharingservicetask", host, spaceID),
+		actionState, sessionState.LogEntry, nil,
+	)
+	fillAppMapFromItemRequest(sessionState, actionState, itemsReq, false)
 }
 
 func (settings DestinationSpace) ResolveDestinationSpace(sessionState *session.State, actionState *action.State, host string) (*elasticstructs.Space, error) {
