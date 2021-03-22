03575a8386b5387ed090db47abeeb4344338ee7e
Daniel Larsson <dlo@qlik.com>
2021-03-22 16:42:26 +0100

    init artifactory map and rest logic

--- scenario/openhub.go
+++ scenario/openhub.go
@@ -1,6 +1,8 @@
 package scenario
 
 import (
+	"fmt"
+
 	"github.com/qlik-oss/gopherciser/action"
 	"github.com/qlik-oss/gopherciser/connection"
 	"github.com/qlik-oss/gopherciser/logger"
@@ -19,9 +21,24 @@ func (openHub OpenHubSettings) Validate() error {
 
 // Execute execute the action
 func (openHub OpenHubSettings) Execute(sessionState *session.State, actionState *action.State, connectionSettings *connection.ConnectionSettings, label string, setHubStart func()) {
+	// New hub connection, clear any existing apps.
+	sessionState.ArtifactMap = session.NewArtifactMap()
 
-	// TODO everything
+	host, err := connectionSettings.GetRestUrl()
+	if err != nil {
+		actionState.AddErrors(err)
+		return
+	}
 
+	// Try one request first to minimize amount of errors when connection fails.
+	_, _ = sessionState.Rest.GetSync(fmt.Sprintf("%s/api/v1/users/me", host), actionState, sessionState.LogEntry, nil)
+	if actionState.Failed {
+		return
+	}
+
+	// TODO - A lot!
+
+	sessionState.Wait(actionState)
 	if err := sessionState.ArtifactMap.LogMap(sessionState.LogEntry); err != nil {
 		sessionState.LogEntry.Log(logger.WarningLevel, err)
 	}
