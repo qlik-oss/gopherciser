1e1536a3677e239f5eb52609e504b53760d17317
Daniel Larsson <dlo@qlik.com>
2021-05-11 11:59:52 +0200

    read users from file

--- users/circularfile.go
+++ users/circularfile.go
@@ -1,16 +1,32 @@
 package users
 
 import (
+	"fmt"
+	"strings"
+	"sync"
+
 	"github.com/pkg/errors"
 	"github.com/qlik-oss/gopherciser/helpers"
 )
 
 type (
-	CircularUsersFile struct {
+	CircularUsersFileCore struct {
 		Filename  helpers.RowFile `json:"filename" displayname:"Filename"`
 		Password  Password        `json:"password,omitempty" displayname:"Password"`
 		Directory string          `json:"directory,omitempty" displayname:"User directory"`
 	}
+
+	CircularUsersFile struct {
+		CircularUsersFileCore
+
+		userList []*User
+		mu       sync.Mutex
+		fill     sync.Once
+	}
+)
+
+const (
+	DefaultUserfileSeparator = ";"
 )
 
 // NewCircularUsersFromFile populate user list from file
@@ -18,26 +34,79 @@ func NewCircularUsersFromFile() *CircularUsersFile {
 	return &CircularUsersFile{}
 }
 
+// UnmarshalJSON CircularUsersFile
+func (users *CircularUsersFile) UnmarshalJSON(arg []byte) error {
+	if err := jsonit.Unmarshal(arg, &users.CircularUsersFileCore); err != nil {
+		return err
+	}
+	if err := users.parseUserList(); err != nil {
+		return err
+	}
+
+	for _, user := range users.userList {
+		fmt.Printf("User<%s> Directory<%s> Password<%s>\n", user.UserName, user.Directory, string(user.Password))
+	}
+
+	return nil
+}
+
 // Validate CircularUsersFile settings
 func (users *CircularUsersFile) Validate() error {
 	if users.Filename.IsEmpty() {
 		return errors.Errorf("filename required for mode<%s>", UserGeneratorCircularFile)
 	}
-	if len(users.Filename.Rows()) < 1 {
+	if len(users.userList) < 1 && len(users.Filename.Rows()) < 1 {
 		return errors.Errorf("filename<%s> has no user names", users.Filename)
 	}
-
-	// TODO validate each row conforms?
-	return nil
+	return users.parseUserList()
 }
 
 // Iterate users from file
 func (users *CircularUsersFile) Iterate(iteration uint64) *User {
-	userRow := users.Filename.Rows()[(iteration-1)%uint64(len(users.Filename.Rows()))]
+	return users.userList[(iteration-1)%uint64(len(users.userList))]
+}
 
-	return &User{
-		UserName:  userRow,
-		Password:  users.Password,
-		Directory: users.Directory,
+func (users *CircularUsersFile) parseUserList() error {
+	var errParse error
+	users.fill.Do(func() {
+		users.mu.Lock()
+		defer users.mu.Unlock()
+		rows := users.Filename.Rows()
+		users.userList = make([]*User, 0, len(rows))
+		for i, row := range rows {
+			user, err := parseRow(row, users.Directory, users.Password)
+			if err != nil {
+				errParse = errors.Wrapf(err, "row:%d<%s> not correctly formated", i+1, row)
+				return
+			}
+			users.userList = append(users.userList, user)
+		}
+		users.Filename.PurgeRows()
+	})
+	return errParse
+}
+
+func parseRow(row, defaultDirectory string, defaultPassword Password) (*User, error) {
+	if row == "" {
+		return nil, errors.New("row is empty")
 	}
+
+	subStrs := strings.SplitN(row, DefaultUserfileSeparator, 3)
+	user := &User{
+		UserName: subStrs[0],
+	}
+
+	if len(subStrs) > 1 && len(subStrs[1]) > 0 {
+		user.Directory = subStrs[1]
+	} else {
+		user.Directory = defaultDirectory
+	}
+
+	if len(subStrs) > 2 && len(subStrs[2]) > 0 {
+		user.Password = Password(subStrs[2])
+	} else {
+		user.Password = defaultPassword
+	}
+
+	return user, nil
 }
