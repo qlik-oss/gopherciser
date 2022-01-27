60dd3651ba262564ccc6af815fa998718fe68c72
Anton Ljungberg <anton.ljungberg@qlik.com>
2022-01-27 11:55:22 +0100

    Save

--- scenario/smartsearch.go
+++ scenario/smartsearch.go
@@ -10,7 +10,9 @@ import (
 )
 
 type (
-	SmartSearchSettings struct{}
+	SmartSearchSettings struct {
+		SearchTerm string `json:"searchterm"`
+	}
 )
 
 // Validate implements ActionSettings interface
@@ -18,21 +20,45 @@ func (settings SmartSearchSettings) Validate() ([]string, error) {
 	return nil, nil
 }
 
+func intPtr(i int) *int {
+	return &i
+}
+
 // Execute implements ActionSettings interface
 func (settings SmartSearchSettings) Execute(sessionState *session.State, actionState *action.State, connection *connection.ConnectionSettings, label string, reset func()) {
 	uplink := sessionState.Connection.Sense()
 	doc := uplink.CurrentApp.Doc
-	doc.SearchSuggest(
-		context.TODO(),
-		&enigma.SearchCombinationOptions{},
-		[]string{""},
-	)
-	doc.SearchResults(
-		context.TODO(),
-		&enigma.SearchCombinationOptions{},
-		[]string{""},
-		&enigma.SearchPage{},
-	)
-	return
+	searchTerms := []string{settings.SearchTerm}
 
+	sessionState.SendRequest(actionState, func(ctx context.Context) error {
+		searchSuggestionResult, err := doc.SearchSuggest(
+			context.TODO(),
+			&enigma.SearchCombinationOptions{},
+			searchTerms,
+		)
+		return err
+	})
+
+	sessionState.SendRequest(actionState, func(ctx context.Context) error {
+		searchResult, err := doc.SearchResults(
+			ctx,
+			&enigma.SearchCombinationOptions{
+				Context:      "CurrentSelections",
+				CharEncoding: "Utf16",
+			},
+			searchTerms,
+			&enigma.SearchPage{
+				Offset: 0,
+				Count:  5,
+				GroupOptions: []*enigma.SearchGroupOptions{
+					{
+						GroupType: "DatasetType",
+						Offset:    0,
+						Count:     intPtr(-1),
+					},
+				},
+			},
+		)
+		return err
+	})
 }
