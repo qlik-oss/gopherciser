e1c6d611eda7124ce13e109087738195367ccfe2
Anton Ljungberg <anton.ljungberg@qlik.com>
2022-02-01 17:05:45 +0100

    Add custom search term parser

--- scenario/smartsearch.go
+++ scenario/smartsearch.go
@@ -2,29 +2,127 @@ package scenario
 
 import (
 	"context"
+	"fmt"
+	"regexp"
 	"strings"
 	"sync"
+	"unicode"
 
+	"github.com/goccy/go-json"
 	"github.com/pkg/errors"
 	"github.com/qlik-oss/enigma-go/v3"
 	"github.com/qlik-oss/gopherciser/action"
 	"github.com/qlik-oss/gopherciser/connection"
+	"github.com/qlik-oss/gopherciser/logger"
 	"github.com/qlik-oss/gopherciser/session"
 )
 
 type (
 	SmartSearchSettings struct {
+		SearchTerms []string
+		SmartSearchSettingsCore
+	}
+
+	SmartSearchSettingsCore struct {
 		SearchText string `json:"searchtext"`
 	}
 )
 
+func (settings *SmartSearchSettings) UnmarshalJSON(bytes []byte) error {
+	err := json.Unmarshal(bytes, &settings.SmartSearchSettingsCore)
+	if err != nil {
+		return err
+	}
+	settings.SearchTerms = parseSearchTerms(settings.SearchText)
+	return nil
+}
+
 // Validate implements ActionSettings interface
 func (settings SmartSearchSettings) Validate() ([]string, error) {
-	return nil, nil
+	var warnings []string
+	if len(settings.SearchTerms) == 0 {
+		return warnings, errors.New("no search terms")
+	}
+	return warnings, nil
 }
 
 func intPtr(i int) *int {
 	return &i
+}
+
+func quote(s string) string {
+	return fmt.Sprintf(`"%s"`, s)
+}
+
+func ternary(cond bool, ifCondTrue string, ifCondFalse string) string {
+	if cond {
+		return ifCondTrue
+	}
+	return ifCondFalse
+
+}
+
+func logSmartSearchResults(logEntry *logger.LogEntry, searchResult *enigma.SearchResult, searchSuggestionResult *enigma.SearchSuggestionResult) {
+	if !logEntry.ShouldLogDebug() {
+		return
+	}
+	suggestions := []string{}
+	for _, suggestion := range searchSuggestionResult.Suggestions {
+		suggestions = append(suggestions, quote(suggestion.Value))
+	}
+	if len(suggestions) > 0 {
+		logEntry.LogDebugf("search suggestions: %s", strings.Join(suggestions, ","))
+	}
+	for _, sga := range searchResult.SearchGroupArray {
+		searchTermsMatched := []string{}
+		for _, stIndex := range sga.SearchTermsMatched {
+			searchTermsMatched = append(searchTermsMatched, quote(searchResult.SearchTerms[stIndex]))
+		}
+		for _, item := range sga.Items {
+			matches := []string{}
+			for _, match := range item.ItemMatches {
+				matches = append(matches, quote(match.Text))
+			}
+			logEntry.LogDebugf(`search result for term%s %s in %s %s: %s`,
+				ternary(len(searchTermsMatched) != 1, "s", ""), strings.Join(searchTermsMatched, ","),
+				item.ItemType, quote(item.Identifier), strings.Join(matches, ","))
+		}
+	}
+}
+
+var spaceRegex = regexp.MustCompile(`\s+`)
+
+func parseSearchTerms(s string) []string {
+	searchTerms := []string{}
+	sb := &strings.Builder{}
+	quoted := false
+	escaped := false
+	addSearchTerm := func() {
+		defer sb.Reset()
+		searchTerm := sb.String()
+		searchTerm = strings.TrimSpace(searchTerm)
+		searchTerm = spaceRegex.ReplaceAllString(searchTerm, " ")
+		if searchTerm != "" {
+			searchTerms = append(searchTerms, searchTerm)
+		}
+	}
+	for _, r := range s {
+		if escaped {
+			sb.WriteRune(r)
+		} else if r == '\\' {
+			escaped = true
+			continue
+		} else if r == '"' {
+			quoted = !quoted
+		} else if !quoted && unicode.IsSpace(r) {
+			addSearchTerm()
+		} else {
+			sb.WriteRune(r)
+		}
+		escaped = false
+	}
+	addSearchTerm()
+	return searchTerms
 }
 
 // Execute implements ActionSettings interface
@@ -35,7 +133,8 @@ func (settings SmartSearchSettings) Execute(sessionState *session.State, actionS
 		return
 	}
 	doc := uplink.CurrentApp.Doc
-	searchTerms := strings.Fields(settings.SearchText)
+	searchTerms := settings.SearchTerms
+	sessionState.LogEntry.LogDebugf(`search terms: "%s"`, strings.Join(searchTerms, `","`))
 
 	var searchResult *enigma.SearchResult
 	var searchSuggestionResult *enigma.SearchSuggestionResult
@@ -96,23 +195,6 @@ func (settings SmartSearchSettings) Execute(sessionState *session.State, actionS
 	}()
 
 	wg.Wait()
-	if sessionState.LogEntry.ShouldLogDebug() {
-		suggestions := []string{}
-
-		for _, suggestion := range searchSuggestionResult.Suggestions {
-			suggestions = append(suggestions, suggestion.Value)
-		}
-		sessionState.LogEntry.LogDebugf("smart search suggestions: %s", strings.Join(suggestions, ", "))
-
-		for _, sga := range searchResult.SearchGroupArray {
-			for _, item := range sga.Items {
-				for _, match := range item.ItemMatches {
-					sessionState.LogEntry.LogDebugf("smart search result: (%s, %s, %s)", item.ItemType, item.Identifier, match.Text)
-				}
-			}
-		}
-
-	}
-
+	logSmartSearchResults(sessionState.LogEntry, searchResult, searchSuggestionResult)
 	sessionState.Wait(actionState)
 }
