ed75b448ddce4c80e0fd305a33f77490b94ad188
Anton Ljungberg <anton.ljungberg@qlik.com>
2022-02-01 11:51:50 +0100

    Do asyncrounous rpc calls

--- scenario/smartsearch.go
+++ scenario/smartsearch.go
@@ -2,7 +2,10 @@ package scenario
 
 import (
 	"context"
+	"strings"
+	"sync"
 
+	"github.com/pkg/errors"
 	"github.com/qlik-oss/enigma-go/v3"
 	"github.com/qlik-oss/gopherciser/action"
 	"github.com/qlik-oss/gopherciser/connection"
@@ -11,7 +14,7 @@ import (
 
 type (
 	SmartSearchSettings struct {
-		SearchTerm string `json:"searchterm"`
+		SearchText string `json:"searchtext"`
 	}
 )
 
@@ -27,38 +30,89 @@ func intPtr(i int) *int {
 // Execute implements ActionSettings interface
 func (settings SmartSearchSettings) Execute(sessionState *session.State, actionState *action.State, connection *connection.ConnectionSettings, label string, reset func()) {
 	uplink := sessionState.Connection.Sense()
+	if uplink.CurrentApp == nil {
+		actionState.AddErrors(errors.New("not connected to app"))
+		return
+	}
 	doc := uplink.CurrentApp.Doc
-	searchTerms := []string{settings.SearchTerm}
+	searchTerms := strings.Fields(settings.SearchText)
 
-	sessionState.SendRequest(actionState, func(ctx context.Context) error {
-		searchSuggestionResult, err := doc.SearchSuggest(
-			context.TODO(),
-			&enigma.SearchCombinationOptions{},
-			searchTerms,
-		)
-		return err
-	})
+	var searchResult *enigma.SearchResult
+	var searchSuggestionResult *enigma.SearchSuggestionResult
+	var wg sync.WaitGroup
 
-	sessionState.SendRequest(actionState, func(ctx context.Context) error {
-		searchResult, err := doc.SearchResults(
-			ctx,
-			&enigma.SearchCombinationOptions{
-				Context:      "CurrentSelections",
-				CharEncoding: "Utf16",
-			},
-			searchTerms,
-			&enigma.SearchPage{
-				Offset: 0,
-				Count:  5,
-				GroupOptions: []*enigma.SearchGroupOptions{
-					{
-						GroupType: "DatasetType",
-						Offset:    0,
-						Count:     intPtr(-1),
-					},
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		err := sessionState.SendRequest(actionState, func(ctx context.Context) error {
+			var err error
+			searchSuggestionResult, err = doc.SearchSuggest(
+				ctx,
+				&enigma.SearchCombinationOptions{},
+				searchTerms,
+			)
+			return err
+		})
+		if err != nil {
+			actionState.AddErrors(err)
+		}
+	}()
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		err := sessionState.SendRequest(actionState, func(ctx context.Context) error {
+			var err error
+			searchResult, err = doc.SearchResults(
+				ctx,
+				&enigma.SearchCombinationOptions{
+					Context:      "CurrentSelections",
+					CharEncoding: "Utf16",
 				},
-			},
-		)
-		return err
-	})
+				searchTerms,
+				&enigma.SearchPage{
+					Offset: 0,
+					Count:  5,
+					GroupOptions: []*enigma.SearchGroupOptions{
+						{
+							GroupType: "DatasetType",
+							Offset:    0,
+							Count:     intPtr(-1),
+						},
+					},
+					GroupItemOptions: []*enigma.SearchGroupItemOptions{{
+						GroupItemType: "Field",
+						Offset:        0,
+						Count:         intPtr(5),
+					}},
+				},
+			)
+			return err
+		})
+
+		if err != nil {
+			actionState.AddErrors(err)
+		}
+	}()
+
+	wg.Wait()
+	if sessionState.LogEntry.ShouldLogDebug() {
+		suggestions := []string{}
+
+		for _, suggestion := range searchSuggestionResult.Suggestions {
+			suggestions = append(suggestions, suggestion.Value)
+		}
+		sessionState.LogEntry.LogDebugf("smart search suggestions: %s", strings.Join(suggestions, ", "))
+
+		for _, sga := range searchResult.SearchGroupArray {
+			for _, item := range sga.Items {
+				for _, match := range item.ItemMatches {
+					sessionState.LogEntry.LogDebugf("smart search result: (%s, %s, %s)", item.ItemType, item.Identifier, match.Text)
+				}
+			}
+		}
+
+	}
+
+	sessionState.Wait(actionState)
 }
