6818b2156fb61d358ba73dc624b9659695f8f001
Anton Ljungberg <anton.ljungberg@qlik.com>
2022-02-02 17:19:25 +0100

    Read searchtext from list or file

--- scenario/smartsearch.go
+++ scenario/smartsearch.go
@@ -12,36 +12,94 @@ import (
 	"github.com/qlik-oss/enigma-go/v3"
 	"github.com/qlik-oss/gopherciser/action"
 	"github.com/qlik-oss/gopherciser/connection"
+	"github.com/qlik-oss/gopherciser/enummap"
+	"github.com/qlik-oss/gopherciser/helpers"
 	"github.com/qlik-oss/gopherciser/logger"
 	"github.com/qlik-oss/gopherciser/session"
 )
 
 type (
 	SmartSearchSettings struct {
-		searchTerms []string
+		searchTermsList [][]string
 		SmartSearchSettingsCore
 	}
 
 	SmartSearchSettingsCore struct {
-		SearchText string `json:"searchtext" displayname:"Search Text" doc-key:"smartsearch.searchtext"`
+		SearchTextSource   SearchTextSource `json:"searchtextsource" displayname:"Search Text Source" doc-key:"smartsearch.searchtextsource"`
+		SearchTextList     []string         `json:"searchtextlist" displayname:"Search Text List" doc-key:"smartsearch.searchtextlist"`
+		SearchTextFilePath string           `json:"searchtextfile" displayname:"Search Text File" doc-key:"smartsearch.searchtextfile"`
 	}
+
+	SearchTextSource int
 )
+
+const (
+	SearchTextSourceList SearchTextSource = iota
+	SearchTextSourceFile
+)
+
+var searchTextSourceEnumMap = enummap.NewEnumMapOrPanic(map[string]int{
+	"searchtextlist": int(SearchTextSourceList),
+	"searchtextfile": int(SearchTextSourceFile),
+})
+
+func (SearchTextSource) GetEnumMap() *enummap.EnumMap {
+	return searchTextSourceEnumMap
+}
+
+func (value *SearchTextSource) UnmarshalJSON(arg []byte) error {
+	i, err := value.GetEnumMap().UnMarshal(arg)
+	if err != nil {
+		return errors.Wrap(err, "Failed to unmarshal SearchTextSource")
+	}
+
+	*value = SearchTextSource(i)
+	return nil
+}
+
+func (value SearchTextSource) MarshalJSON() ([]byte, error) {
+	str, err := value.GetEnumMap().String(int(value))
+	if err != nil {
+		return nil, errors.Errorf("Unknown SearchTextSource<%d>", value)
+	}
+	return []byte(fmt.Sprintf(`"%s"`, str)), nil
+}
 
 func (settings *SmartSearchSettings) UnmarshalJSON(bytes []byte) error {
 	err := json.Unmarshal(bytes, &settings.SmartSearchSettingsCore)
 	if err != nil {
 		return err
 	}
-	settings.searchTerms = parseSearchTerms(settings.SearchText)
+	switch settings.SearchTextSource {
+	case SearchTextSourceList:
+	case SearchTextSourceFile:
+		rowFile, err := helpers.NewRowFile(settings.SearchTextFilePath)
+		if err != nil {
+			return err
+		}
+		settings.SearchTextList = rowFile.Rows()
+	default:
+		return errors.Errorf("Unknown SearchTextSource<%d>", settings.SearchTextSource)
+	}
+	for _, searchText := range settings.SearchTextList {
+		searchTerms := parseSearchTerms(searchText)
+		settings.searchTermsList = append(settings.searchTermsList, searchTerms)
+	}
 	return nil
 }
 
 // Validate implements ActionSettings interface
 func (settings SmartSearchSettings) Validate() ([]string, error) {
 	var warnings []string
-	if len(settings.searchTerms) == 0 {
-		return warnings, errors.New("no search terms")
+	if len(settings.searchTermsList) == 0 {
+		return warnings, errors.New("no searchtext found")
 	}
+	for idx, searchTerms := range settings.searchTermsList {
+		if len(searchTerms) == 0 {
+			return warnings, errors.Errorf(`no search terms found in searchtext%d<%s> `, idx+1, settings.SearchTextList[idx])
+		}
+	}
+
 	return warnings, nil
 }
 
@@ -62,19 +120,22 @@ func ternary(cond bool, ifCondTrue string, ifCondFalse string) string {
 }
 
 func logSearchSuggestionResult(logEntry *logger.LogEntry, searchSuggestionResult *enigma.SearchSuggestionResult) {
+	if !logEntry.ShouldLogDebug() {
+		return
+	}
 	suggestions := []string{}
 	for _, suggestion := range searchSuggestionResult.Suggestions {
 		suggestions = append(suggestions, quote(suggestion.Value))
 	}
-	if len(suggestions) > 0 {
-		logEntry.LogDebugf("search suggestions: %s", strings.Join(suggestions, ","))
-	}
+	logEntry.LogDebugf("search suggestions: %s",
+		ternary(len(suggestions) > 0, strings.Join(suggestions, ","), "NONE"))
 }
 
 func logSearchResult(logEntry *logger.LogEntry, searchResult *enigma.SearchResult) {
 	if !logEntry.ShouldLogDebug() {
 		return
 	}
+	hasResult := false
 	for _, sga := range searchResult.SearchGroupArray {
 		searchTermsMatched := []string{}
 		for _, stIndex := range sga.SearchTermsMatched {
@@ -85,10 +146,14 @@ func logSearchResult(logEntry *logger.LogEntry, searchResult *enigma.SearchResul
 			for _, match := range item.ItemMatches {
 				matches = append(matches, quote(match.Text))
 			}
+			hasResult = true
 			logEntry.LogDebugf(`search result for term%s %s in %s %s: %s`,
 				ternary(len(searchTermsMatched) != 1, "s", ""), strings.Join(searchTermsMatched, ","),
 				item.ItemType, quote(item.Identifier), strings.Join(matches, ","))
 		}
+	}
+	if !hasResult {
+		logEntry.LogDebug("search result: NONE")
 	}
 }
 
@@ -135,8 +200,15 @@ func (settings SmartSearchSettings) Execute(sessionState *session.State, actionS
 		return
 	}
 	doc := uplink.CurrentApp.Doc
-	searchTerms := settings.searchTerms
-	sessionState.LogEntry.LogDebugf(`search terms: "%s"`, strings.Join(searchTerms, `","`))
+
+	randomSearchTermsIdx := sessionState.Randomizer().Rand(len(settings.searchTermsList))
+	searchTerms := settings.searchTermsList[randomSearchTermsIdx]
+	searchText := settings.SearchTextList[randomSearchTermsIdx]
+
+	if sessionState.LogEntry.ShouldLogDebug() {
+		sessionState.LogEntry.LogDebugf(`search text: %s`, searchText)
+		sessionState.LogEntry.LogDebugf(`search terms: "%s"`, strings.Join(searchTerms, `","`))
+	}
 
 	sessionState.QueueRequest(func(ctx context.Context) error {
 		searchSuggestionResult, err := doc.SearchSuggest(
