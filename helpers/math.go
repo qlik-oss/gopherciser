033dcb0af0ba34a78c1020ef9adf94d525850231
Daniel Larsson <dlo@qlik.com>
2024-06-17 10:03:20 +0200

    fix spelling

--- helpers/math.go
+++ helpers/math.go
@@ -2,7 +2,7 @@ package helpers
 
 import "golang.org/x/exp/constraints"
 
-// Min returns the least of a'and b
+// Min returns the least of a and b
 func Min[T constraints.Ordered](a, b T) T {
 	if a < b {
 		return a
@@ -10,7 +10,7 @@ func Min[T constraints.Ordered](a, b T) T {
 	return b
 }
 
-// Max returns the greatest of a'and b
+// Max returns the greatest of a and b
 func Max[T constraints.Ordered](a, b T) T {
 	if a > b {
 		return a
