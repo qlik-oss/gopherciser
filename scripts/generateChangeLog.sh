2bd965ad2266ac6928d818d3d3fd8365c19d8721
Daniel Larsson <dlo@qlik.com>
2024-08-06 15:14:02 +0200

    fix generated link

--- scripts/generateChangeLog.sh
+++ scripts/generateChangeLog.sh
@@ -11,5 +11,5 @@ CURVER=$(git describe --match "v[0-9]*")
 PREVVER=$(git describe --match "v[0-9]*" --abbrev=0 HEAD~1)
 
 echo "# Changes $PREVVER -> $CURVER" > "$CHFILE"
-git log "$PREVVER"..@ --pretty=format:'[%h](https://github.com/qlik-trial/gopherciser/commit/%H) %s
+git log "$PREVVER"..@ --pretty=format:'[%h](https://github.com/qlik-oss/gopherciser/commit/%H) %s
 ' >> "$CHFILE"
