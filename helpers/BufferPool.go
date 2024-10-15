d67e3d09e98aecf9de050a91cd760fdb8136ea8b
Daniel Larsson <dlo@qlik.com>
2024-10-15 13:03:35 +0200

    Move ReadAll func and use globalpool buffer for WS messages also

--- helpers/BufferPool.go
+++ helpers/BufferPool.go
@@ -2,6 +2,7 @@ package helpers
 
 import (
 	"bytes"
+	"io"
 	"sync"
 )
 
@@ -28,4 +29,30 @@ func (bp *BufferPool) Get() *bytes.Buffer {
 func (bp *BufferPool) Put(buf *bytes.Buffer) {
 	buf.Reset()
 	bp.pool.Put(buf)
+}
+
+func ReadAll(r io.Reader) ([]byte, error) {
+	buf := GlobalBufferPool.Get()
+	defer GlobalBufferPool.Put(buf)
+
+	capacity := int64(bytes.MinRead)
+	var err error
+	// If the buffer overflows, we will get bytes.ErrTooLarge.
+	// Return that as an error. Any other panic remains.
+	defer func() {
+		e := recover()
+		if e == nil {
+			return
+		}
+		if panicErr, ok := e.(error); ok && panicErr == bytes.ErrTooLarge {
+			err = panicErr
+		} else {
+			panic(e)
+		}
+	}()
+	if int64(int(capacity)) == capacity {
+		buf.Grow(int(capacity))
+	}
+	_, err = buf.ReadFrom(r)
+	return buf.Bytes(), err
 }
