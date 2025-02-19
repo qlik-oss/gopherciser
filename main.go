3be2e5c9c24d09c73fe5257e263f9bf025e4f8a7
Daniel Larsson <dlo@qlik.com>
2025-02-19 16:31:08 +0100

    remove generation of settingup.json via go generate

--- main.go
+++ main.go
@@ -9,9 +9,6 @@ import (
 // Compile documentation data to be used by GUI and for markdown generation
 //go:generate go run ./generatedocs/cmd/compiledocs
 
-// Generate markdown files
-//go:generate go run ./generatedocs/cmd/generatemarkdown --output ./docs/settingup.md
-
 func main() {
 	cmd.Execute()
 }
