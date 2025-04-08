260037ff29507f3ce32304d935c56e6f3e1af014
Daniel Larsson <dlo@qlik.com>
2025-04-08 12:56:49 +0200

    autodetect privatekey type and update default signing algorithm

--- connection/jwtconnection_test.go
+++ connection/jwtconnection_test.go
@@ -10,9 +10,9 @@ import (
 	"encoding/pem"
 	"fmt"
 	"os"
-	"sync"
 	"testing"
 
+	"github.com/goccy/go-json"
 	"github.com/qlik-oss/gopherciser/logger"
 	"github.com/qlik-oss/gopherciser/session"
 	"github.com/qlik-oss/gopherciser/synced"
@@ -20,6 +20,7 @@ import (
 )
 
 type TestAlgos struct {
+	Name       string
 	Alg        string
 	PKeyWriter func(keyfile *os.File) error
 }
@@ -35,21 +36,12 @@ func TestParsing(t *testing.T) {
 	}
 
 	settings := ConnectJWTSettings{
-		Claims: *stClaims,
-	}
-
-	user := &users.User{
-		UserName:  "mytestuser_1",
-		Directory: "mydirectory",
-	}
-
-	sessionState := &session.State{
-		User: user,
-		LogEntry: &logger.LogEntry{
-			Session: &logger.SessionEntry{},
-			Action:  &logger.ActionEntry{},
+		ConnectJWTSettingsCore: ConnectJWTSettingsCore{
+			Claims: *stClaims,
 		},
 	}
+
+	sessionState, user := createSessionAndUserState()
 
 	claims, err := settings.executeClaimsTemplates(sessionState)
 	if err != nil {
@@ -81,7 +73,9 @@ func TestParsing(t *testing.T) {
 
 	// advanced claims
 	settings = ConnectJWTSettings{
-		Claims: *stClaims,
+		ConnectJWTSettingsCore: ConnectJWTSettingsCore{
+			Claims: *stClaims,
+		},
 	}
 
 	claims, err = settings.executeClaimsTemplates(sessionState)
@@ -155,84 +149,120 @@ func TestParsing(t *testing.T) {
 	key = "kid"
 	value = fmt.Sprintf("%v", jwtHeader[key])
 	validate(t, key, value, expected)
-
-	SigningTest(t, sessionState, &settings)
 }
 
-func SigningTest(t *testing.T, sessionState *session.State, settings *ConnectJWTSettings) {
+// func SigningTest(t *testing.T, sessionState *session.State, settings *ConnectJWTSettings) {
+func TestSigning(t *testing.T) {
 	algoTests := []TestAlgos{
 		{
-			Alg:        "",
-			PKeyWriter: func(keyfile *os.File) error { return writeRSAKey(2048, keyfile) },
-		},
-		{
-			Alg:        "RS256",
-			PKeyWriter: func(keyfile *os.File) error { return writeRSAKey(2048, keyfile) },
-		},
-		{
-			Alg:        "RS384",
-			PKeyWriter: func(keyfile *os.File) error { return writeRSAKey(2048, keyfile) },
-		},
-		{
-			Alg:        "RS512",
-			PKeyWriter: func(keyfile *os.File) error { return writeRSAKey(2048, keyfile) },
-		},
-		{
+			Name:       "none",
 			Alg:        "none",
 			PKeyWriter: func(keyfile *os.File) error { return os.WriteFile(keyfile.Name(), []byte{}, 0600) },
 		},
 		{
+			Name:       "RSA-default",
+			Alg:        "",
+			PKeyWriter: func(keyfile *os.File) error { return writeRSAKey(2048, keyfile) },
+		},
+		{
+			Name:       "RS256",
+			Alg:        "RS256",
+			PKeyWriter: func(keyfile *os.File) error { return writeRSAKey(2048, keyfile) },
+		},
+		{
+			Name:       "RS384",
+			Alg:        "RS384",
+			PKeyWriter: func(keyfile *os.File) error { return writeRSAKey(2048, keyfile) },
+		},
+		{
+			Name:       "RS512",
+			Alg:        "RS512",
+			PKeyWriter: func(keyfile *os.File) error { return writeRSAKey(2048, keyfile) },
+		},
+		{
+			Name:       "EdDSA",
 			Alg:        "EdDSA",
 			PKeyWriter: func(keyfile *os.File) error { return writeEdDSAKey(keyfile) },
 		},
 		{
+			Name:       "ES256",
 			Alg:        "ES256",
 			PKeyWriter: func(keyfile *os.File) error { return writeECKey("ES256", keyfile) },
 		},
 		{
+			Name:       "ES256-default",
+			Alg:        "",
+			PKeyWriter: func(keyfile *os.File) error { return writeECKey("ES256", keyfile) },
+		},
+		{
+			Name:       "ES384",
 			Alg:        "ES384",
 			PKeyWriter: func(keyfile *os.File) error { return writeECKey("ES384", keyfile) },
 		},
 		{
+			Name:       "ES384-default",
+			Alg:        "",
+			PKeyWriter: func(keyfile *os.File) error { return writeECKey("ES384", keyfile) },
+		},
+		{
+			Name:       "ES512",
 			Alg:        "ES512",
 			PKeyWriter: func(keyfile *os.File) error { return writeECKey("ES512", keyfile) },
 		},
 		{
+			Name:       "ES512-default",
+			Alg:        "",
+			PKeyWriter: func(keyfile *os.File) error { return writeECKey("ES512", keyfile) },
+		},
+		{
+			Name:       "PS256",
 			Alg:        "PS256",
 			PKeyWriter: func(keyfile *os.File) error { return writeRSAKey(2048, keyfile) },
 		},
 		{
+			Name:       "PS384",
 			Alg:        "PS384",
 			PKeyWriter: func(keyfile *os.File) error { return writeRSAKey(2048, keyfile) },
 		},
 		{
+			Name:       "PS512",
 			Alg:        "PS512",
 			PKeyWriter: func(keyfile *os.File) error { return writeRSAKey(2048, keyfile) },
 		},
 	}
 
 	for _, test := range algoTests {
-		keyfile, err := os.CreateTemp("", "PrivateKey")
-		defer func() {
-			_ = keyfile.Close()
-		}()
-		if err != nil {
-			t.Fatal(err)
-		}
-		settings.KeyPath = keyfile.Name()
-		settings.Alg = test.Alg
-		if err := test.PKeyWriter(keyfile); err != nil {
-			t.Error(err)
-			continue
-		}
-		// rfile, _ := os.ReadFile(keyfile.Name())
-		// t.Logf("key<%s>:\n%s\n", settings.Alg, rfile)
-		settings.readKey = sync.Once{} // make sure to re-read key
-		header, err := settings.GetJwtHeader(sessionState, nil)
-		if err != nil {
-			t.Errorf("GetJwtHeader failed algo<%s>: %v", test.Alg, err)
-		}
-		t.Logf("alg<%s> bearer: %s", test.Alg, header)
+		t.Run(test.Name, func(t *testing.T) {
+			keyfile, err := os.CreateTemp("", "PrivateKey")
+			defer func() {
+				_ = keyfile.Close()
+			}()
+			if err != nil {
+				t.Fatal(err)
+			}
+			if err := test.PKeyWriter(keyfile); err != nil {
+				t.Fatal(err)
+			}
+
+			rawSettings := `{
+				"alg": "` + test.Alg + `",
+				"keypath": "` + keyfile.Name() + `",
+				"claims": "{\"user\":\"{{.UserName}}\",\"directory\":\"{{.Directory}}\"}"
+			}`
+
+			var settings ConnectJWTSettings
+			if err := json.Unmarshal([]byte(rawSettings), &settings); err != nil {
+				t.Fatal(err)
+			}
+
+			sessionState, _ := createSessionAndUserState()
+
+			header, err := settings.GetJwtHeader(sessionState, nil)
+			if err != nil {
+				t.Fatalf("GetJwtHeader failed algo<%s>: %v", test.Alg, err)
+			}
+			t.Logf("alg<%s> bearer: %s", test.Alg, header)
+		})
 	}
 }
 
@@ -308,4 +338,19 @@ func validate(t *testing.T, key, value, expected string) {
 	if value != expected {
 		t.Errorf("key<%s> expected<%s> got<%s>", key, expected, value)
 	}
+}
+
+func createSessionAndUserState() (*session.State, *users.User) {
+	user := &users.User{
+		UserName:  "mytestuser_1",
+		Directory: "mydirectory",
+	}
+
+	return &session.State{
+		User: user,
+		LogEntry: &logger.LogEntry{
+			Session: &logger.SessionEntry{},
+			Action:  &logger.ActionEntry{},
+		},
+	}, user
 }
