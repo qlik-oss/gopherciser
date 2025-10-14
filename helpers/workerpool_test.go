eb1df16bb7d2cb107041f1d27701f96d10ca6a24
Daniel Larsson <dlo@qlik.com>
2025-10-14 16:50:42 +0200

    use synctest

--- helpers/workerpool_test.go
+++ helpers/workerpool_test.go
@@ -2,32 +2,38 @@ package helpers_test
 
 import (
 	"testing"
+	"testing/synctest"
+	"time"
 
 	"github.com/qlik-oss/gopherciser/helpers"
 )
 
 func TestWorkerPool(t *testing.T) {
-	total := 10
-	concurrency := 2
-	pool, err := helpers.NewWorkerPool(concurrency, total)
-	if err != nil {
-		t.Fatal(err)
-	}
-	for range total {
-		if err := pool.AddTask(func() error {
-			return nil
-		}); err != nil {
-			t.Error(err)
+	synctest.Test(t, func(t *testing.T) {
+		total := 10
+		concurrency := 2
+		pool, err := helpers.NewWorkerPool(concurrency, total)
+		if err != nil {
+			t.Fatal(err)
 		}
-	}
-	totalResult := 0
-	for result := range pool.Results() {
-		totalResult++
-		if result != nil {
-			t.Error(result)
+		for i := range total {
+			if err := pool.AddTask(func() error {
+				time.Sleep(time.Duration(i) * time.Second)
+				return nil
+			}); err != nil {
+				t.Error(err)
+			}
 		}
-	}
-	if totalResult != total {
-		t.Errorf("total results<%d> expected<%d>", totalResult, total)
-	}
+		totalResult := 0
+		for result := range pool.Results() {
+			totalResult++
+			if result != nil {
+				t.Error(result)
+			}
+		}
+		if totalResult != total {
+			t.Errorf("total results<%d> expected<%d>", totalResult, total)
+		}
+	})
+
 }
