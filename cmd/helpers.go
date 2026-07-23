6765b222125eb5673eb1adeb3dc99b059aca6876
Daniel Larsson <dlo@qlik.com>
2026-07-23 11:44:03 +0200

    remove usage of go-ps

--- cmd/helpers.go
+++ cmd/helpers.go
@@ -10,7 +10,6 @@ import (
 	"time"
 
 	"github.com/goccy/go-json"
-	"github.com/mitchellh/go-ps"
 	"github.com/pkg/errors"
 	"github.com/qlik-oss/gopherciser/config"
 	"github.com/qlik-oss/gopherciser/helpers"
@@ -64,11 +63,9 @@ func UnmarshalConfigFile() (*config.Config, error) {
 	var cfgJSON []byte
 	var hasPipe bool
 
-	if !IsLaunchedByDebugger() {
-		hasPipe, err = HasPipe()
-		if err != nil {
-			return nil, errors.Wrap(err, "error discovering if piped data exist")
-		}
+	hasPipe, err = HasPipe()
+	if err != nil {
+		return nil, errors.Wrap(err, "error discovering if piped data exist")
 	}
 
 	if cfgFile == "" {
@@ -281,18 +278,4 @@ func HasPipe() (bool, error) {
 		return false, errors.WithStack(err)
 	}
 	return fileInfo.Mode()&os.ModeCharDevice == 0, nil
-}
-
-// IsLaunchedByDebugger discovers if pararent process is deleve
-func IsLaunchedByDebugger() bool {
-	parent, err := ps.FindProcess(os.Getppid())
-	if err != nil {
-		return false
-	}
-	name := parent.Executable()
-	switch name {
-	case "dlv", "dlv.exe", "debugserver", "dlv-dap":
-		return true
-	}
-	return false
 }
--- go.mod
+++ go.mod
@@ -12,7 +12,6 @@ require (
 	github.com/google/uuid v1.6.0
 	github.com/hashicorp/go-multierror v1.1.1
 	github.com/hashicorp/go-version v1.9.0
-	github.com/mitchellh/go-ps v1.0.0
 	github.com/pkg/errors v0.9.1
 	github.com/prometheus/client_golang v1.23.2
 	github.com/qlik-oss/enigma-go/v4 v4.5.0
